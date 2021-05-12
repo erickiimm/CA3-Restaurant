97a823e06ad464eee2604ad44bfe8b9fb4f11c29
Eric Mabe <erickiimm@gmail.com>
2021-05-12 18:23:51 +0100

    scripts js commit

--- scripts.js
+++ scripts.js
@@ -11,3 +11,23 @@
         passwordOk = false;
         hints.push("The password must have at least one lowercase letter");
     }
+    //uppercase letter item
+    if (!(/[A-Z]/.test(inputValue))) {        
+        passwordOk = false;
+        hints.push("The password must have at least one uppercase letter");
+    }
+    //special character item
+    if (!(/[ `!@#$%^&*()_+\-=[\]{};":"\\|,.<>/?~]/.test(inputValue))) {        
+        passwordOk = false;
+        hints.push("It must contain one digit and one special character");
+    } 
+    //delimit password lenght 
+    if (inputValue.length < 8) {       
+        passwordOk = false;
+        hints.push("It must be at least eight characters long");
+    }
+    ////get error if the staff not follow the password rules 
+    if (passwordOk === false) {
+        var errorElement = document.getElementById("error-message");
+        var errorText = document.createTextNode(`*Invalid format, please reenter. (${hints.join(", ")})`);
+        errorElement.appendChild(errorText);
