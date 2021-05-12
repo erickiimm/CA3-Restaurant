4294ce1ea03e54265991e0d675f2fe4b14c6b383
Eric Mabe <erickiimm@gmail.com>
2021-05-12 18:19:36 +0100

    script js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@
     var inputValue = document.getElementById("password").value;
     document.getElementById("error-message").textContent = "";
     var passwordOk = true;
+    var hints = [];
+    //Staff password validator :
+    //
+    //lowercase letter item 
+    if (!(/[a-z]/.test(inputValue))) {        
+        passwordOk = false;
+        hints.push("The password must have at least one lowercase letter");
